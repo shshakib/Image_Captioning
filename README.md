895ef9f243f8044527cb6c05b3fab6f5b8b2c273
Shaya Sh <shaya.shakib@gmail.com>
2024-11-17 21:59:58 -0500

    Fix some bugs, implement scores, early stop in training loop, weight decay

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ References and Acknowledgments:
 
 This project was developed using knowledge, code, and/or resources from the following references:
 https://www.kaggle.com/code/mdteach/image-captioning-with-attention-pytorch
+
 https://github.com/tylin/coco-caption/tree/master/pycocoevalcap/meteor
+
 https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ import os
 import re
 from nltk.translate.bleu_score import corpus_bleu
 from pycocoevalcap.cider.cider import Cider
-from pycocoevalcap.meteor.meteor import Meteor
+#from pycocoevalcap.meteor.meteor import Meteor
+from nltk.translate.meteor_score import meteor_score
+import nltk
 
 
 def denormalize_img(img_tensor, mean, std):
@@ -70,8 +72,40 @@ def plot_metrics(train_losses, val_losses, bleu_scores, cider_scores, meteor_sco
     plt.show()
 
 
+def load_model(model, optimizer, checkpoint_path, learning_rate=None, device='cpu'):
+    """
+    Load a saved model from specified checkpoint.
+    """
+    if not os.path.exists(checkpoint_path):
+        raise FileNotFoundError(f"Checkpoint not found at {checkpoint_path}")
+    
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    print(f"Loaded model from epoch {epoch}")
+    
+    # Ensure optimizer uses the correct learning rate
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = learning_rate
+    
+    return model, optimizer, epoch
 
-def calculate_bleu_score(model, data_loader, vocab, device):
+
+def calculate_bleu_score(model, data_loader, vocab, vocab_builder, device):
+    """
+    Calculate BLEU score
+
+    Parameters:
+        model: Trained model to evaluate.
+        data_loader: DataLoader for validation/test data.
+        vocab: Vocab for generating captions.
+        vocab_builder: An instance of VocabularyBuilder class to use the tokenizer.
+        device: Device (CPU or GPU).
+
+    Returns:
+        float: Corpus BLEU score.
+    """
     model.eval()
     references, hypotheses = [], []
     
@@ -83,9 +117,14 @@ def calculate_bleu_score(model, data_loader, vocab, device):
                 features = model.encoder(val_images[i:i+1])
                 generated_caption, _ = model.decoder.generate_caption(features, vocab=vocab)
 
-                #Generated and reference captions
-                hypotheses.append(generated_caption)
-                references.append([val_raw_captions[i].split()])
+                tokenized_hypothesis = [
+                    word.lower() for word in generated_caption if word not in ["<unk>", "<eos>"]
+                ]
+
+                tokenized_reference = vocab_builder.spacy_tokenizer(val_raw_captions[i])
+
+                hypotheses.append(tokenized_hypothesis)
+                references.append([tokenized_reference])
 
     bleu_score = corpus_bleu(references, hypotheses)
     return bleu_score
@@ -112,7 +151,20 @@ def calculate_loss(model, data_loader, criterion, vocab_size, device):
 
 
 
-def calculate_cider_score(model, data_loader, vocab, device):
+def calculate_cider_score(model, data_loader, vocab, vocab_builder, device):
+    """
+    Calculate CIDEr score using pycocoevalcap.
+
+    Parameters:
+        model: Trained model to evaluate.
+        data_loader: DataLoader providing validation/test data.
+        vocab: Vocab used for generating captions.
+        vocab_builder: Instance of VocabularyBuilder class to use the tokenizer.
+        device: Device (CPU or GPU).
+
+    Returns:
+        float: Average CIDEr score for the dataset.
+    """
     model.eval()
     references_dict, hypotheses_dict = {}, {}
     
@@ -121,17 +173,75 @@ def calculate_cider_score(model, data_loader, vocab, device):
             val_images = val_images.to(device)
 
             for i in range(val_images.size(0)):
+
                 features = model.encoder(val_images[i:i+1])
                 generated_caption, _ = model.decoder.generate_caption(features, vocab=vocab)
 
+                cleaned_caption = [word for word in generated_caption if word not in ["<unk>", "<eos>"]]
+
                 image_id = idx * val_images.size(0) + i
-                hypotheses_dict[image_id] = [' '.join(generated_caption)]
-                references_dict[image_id] = [' '.join(c.split()) for c in val_raw_captions[i]]
+                hypotheses_dict[image_id] = [' '.join(cleaned_caption)]
 
-    #Pycocoevalcap for computing CIDEr
+                references_dict[image_id] = [' '.join(vocab_builder.spacy_tokenizer(val_raw_captions[i]))]
+
     cider_scorer = Cider()
-    _, cider_score = cider_scorer.compute_score(references_dict, hypotheses_dict)
-    return cider_score
+    avg_cider_score, _ = cider_scorer.compute_score(references_dict, hypotheses_dict)
+    return avg_cider_score
 
 
 
+def calculate_meteor_score(model, data_loader, vocab, vocab_builder, device):
+    """
+    Calculate METEOR score.
+
+    Parameters:
+        model: Trained model
+        data_loader: DataLoader for test/validation data.
+        vocab: Vocab for generating captions.
+        vocab_builder: Instance of VocabularyBuilder class to use for tokenizer.
+        device: Device (CPU or GPU).
+
+    Returns:
+        float: Average METEOR score.
+    """
+    model.eval()
+    references, hypotheses = [], []
+    
+    with torch.no_grad():
+        for val_images, val_caption_token_ids, val_raw_captions in data_loader:
+            val_images = val_images.to(device)
+
+            for i in range(val_images.size(0)):
+                features = model.encoder(val_images[i:i+1])
+                generated_caption, _ = model.decoder.generate_caption(features, vocab=vocab)
+
+                tokenized_hypothesis = [word for word in generated_caption if word not in ["<unk>", "<eos>"]]#remove eos and unk
+                tokenized_reference = vocab_builder.spacy_tokenizer(val_raw_captions[i])
+                references.append(tokenized_reference)
+                hypotheses.append(tokenized_hypothesis)
+
+    meteor_scores = [
+        meteor_score([ref], hyp) for ref, hyp in zip(references, hypotheses)
+    ]
+    avg_meteor_score = sum(meteor_scores) / len(meteor_scores)
+    return avg_meteor_score
+
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0):
+        """
+        Early stopping to stop training when validation loss doesn't improve.
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.best_loss = float('inf')
+        self.counter = 0
+
+    def __call__(self, val_loss):
+        if val_loss < self.best_loss - self.min_delta:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+        return self.counter > self.patience
